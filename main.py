60d410c39047298bec9ce507ca37d8dc030d8586
Himanshu Raj <HRaj@ABS-CBN.COM>
2023-11-11 12:42:30 +0530

    added template for video download from youtube

---
+++ main.py
@@ -0,0 +1,51 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request, send_from_directory, render_template
+from download_scripts import DownloadYTVideo
+from flask_cors import CORS
+import os
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+CORS(app)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/', methods=['POST', 'GET'])
+def home():
+	if(request.method == "GET"):
+		try:
+			return render_template('index.html')
+		except Exception as e:
+			print("Error in rendering template", e)
+	elif(request.method == "POST"):
+		print("THIS IS POST")
+		print(request.form)
+		video_url=request.form['video_url']
+		print("VIDEO_URL", video_url)
+		return DownloadYTVideo(video_url)
+
+
+
+@app.route('/downloadyoutubevideo', methods=['POST', 'GET'])
+# ‘/’ URL is bound with hello_world() function.
+def download_yt_video():
+	try:
+		print(request.json['url'])
+		yturl=request.json["url"]
+		# return { "download_url": DownloadYTVideo(yturl)}
+		return DownloadYTVideo(yturl)
+	except Exception as er:
+		print("Error", er)
+
+
+
+
+# main driver function
+if __name__ == '__main__':
+
+	# run() method of Flask class runs the application 
+	# on the local development server.
+	app.run(host="0.0.0.0", port=5000)
